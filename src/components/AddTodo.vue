5691694a7e3df75386a415ce9d1f5f21348492b0
Garry Tiscovschi <tiscovsg@tcd.ie>
2020-10-11 19:01:11 +0100

    Initial commit

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <!-- first construct event then emit it up -->
+    <form @submit = "AddTodo">
+      <input type="text" name="inputTodo" v-model ="inputTodo" placeholder="Add Todo...">
+      <input type="submit" value="submit" class = "btn">
+    </form>
+  </div>
+</template>
+
+<script>
+//import { v4 as uuidv4 } from 'uuid';
+export default {
+  name: "Add Todo",
+  data() {
+    return{
+      inputTodo: ''
+    }
+  },
+  methods: {
+    AddTodo(e) {
+      e.preventDefault();
+      const newTodo = {
+
+        title: this.inputTodo,
+        completed: false
+      }
+      //send to parent.
+      this.$emit('add-todo', newTodo);
+
+      this.inputTodo = '';
+    }
+  }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+input[type="submit"] {
+  flex: 2;
+}
+</style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class = "header">
+    <h1>Todo List</h1>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: "Header"
+  }
+</script>
+
+<style scoped>
+.header {
+  background: #555;
+  color: #fff;
+  text-align: center;
+  padding: 10px;
+  border-radius: 25px;
+}
+.header a {
+  color: #fff;
+  padding-right: 5px;
+  text-decoration: none;
+}
+</style>
